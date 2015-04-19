f6294f23a7b654c4109e6cbe1cf5f533297249c4
omar manrique <omar.manrique@gmail.com>
2015-04-19 14:29:50 +0200

    os en play music

--- MusicFeelings/app/src/main/java/com/twoc15/traity/musicfeelings/PlayMusic.java
+++ MusicFeelings/app/src/main/java/com/twoc15/traity/musicfeelings/PlayMusic.java
@@ -1,17 +1,96 @@
 package com.twoc15.traity.musicfeelings;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.twoc15.traity.musicfeelings.adapters.TagsAdapter;
+import com.twoc15.traity.musicfeelings.helpers.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PlayMusic extends ActionBarActivity {
+    private TagsAdapter adapter = null;
+    private ListView lista;
+    private MediaPlayer mMediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
+        Log.d(Constants.STR_LOG_TAG, "start new");
+
+        lista = (ListView) findViewById(R.id.listView);
+
+        List<String> listaTag = new ArrayList<String>();
+        listaTag.add("rosinante");
+        listaTag.add("dulceinea");
+        listaTag.add("sancho");
+
+        adapter = new TagsAdapter(getApplicationContext(), R.layout.tag_item, listaTag, false);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textViewItem = (TextView) view.findViewById(R.id.textTag);
+                StringBuffer tagText = new StringBuffer(textViewItem.getText());
+
+                Log.d(Constants.STR_LOG_TAG, new StringBuffer("Item is: ").append(position).append(" - ").append(textViewItem.getText()).toString());
+
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH);
+                intent.putExtra(MediaStore.EXTRA_MEDIA_FOCUS, MediaStore.Audio.Genres.ENTRY_CONTENT_TYPE);
+                intent.putExtra(SearchManager.QUERY, tagText.toString());
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        /**
+         *
+         Intent intent = new Intent();
+         intent.setAction(android.content.Intent.ACTION_VIEW);
+         File file = new File("/sdcard/test.mp3");
+         intent.setDataAndType(Uri.fromFile(file), "audio/*");
+         startActivity(intent);
+         *
+         */
+
+        /**
+         *
+         String tagsStr = PreferenceManagerHelperImp.getInstance(this).getPreferenceValue(Constants.STR_PREF_TAGS);
+
+         new AsyncTaskMW<String, ArrayAdapter<String>, ListView>("pepe, luis, ian", lista) {
+
+        @Override
+        protected ArrayAdapter<String> process(String path) {
+        Log.d(Constants.STR_LOG_TAG, "start task");
+        adapter1 = new ArrayAdapter<String>(getApplicationContext(), R.layout.tag_item, path.split(","));
+        adapter1.setNotifyOnChange(true);
+        return adapter1;
+        }
+
+        @Override
+        protected void applyOutputToTarget(ArrayAdapter<String> result, ListView target) {
+        Log.d(Constants.STR_LOG_TAG, "end task");
+        target.setAdapter(result);
+        }
+        }.execute();
+         */
+
     }
 
 
@@ -36,4 +115,6 @@ public class PlayMusic extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
+
--- MusicFeelings/app/src/main/java/com/twoc15/traity/musicfeelings/helpers/Constants.java
+++ MusicFeelings/app/src/main/java/com/twoc15/traity/musicfeelings/helpers/Constants.java
@@ -3,5 +3,6 @@ package com.twoc15.traity.musicfeelings.helpers;
 public class Constants {
 	public final static String STR_PREF_TAGS = "TAGS";
 	public final static String STRING_NOTAVAILABLE = "NA";
+	public final static String STR_LOG_TAG = "MUSIC_FEELING";
 
 }
